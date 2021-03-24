552d63930aacc4550ef636fa253f7440958cab71
Soham De Roy <sohamderoy.iitk@gmail.com>
2021-03-24 00:41:04 -0700

    Add files via upload

---
+++ precache-manifest.68e6189b70fa5d869d09a47844186c7b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff665e0309161d1f2882b742d7de6c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4615d9ba863d19d19ef",
+    "url": "/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "f1d3c41afde386cd6150",
+    "url": "/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "775ec241fd3cffa4c9d6",
+    "url": "/static/css/main.7b9e28cb.chunk.css"
+  },
+  {
+    "revision": "4566388b38da4492731f",
+    "url": "/static/js/2.86587587.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.86587587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4615d9ba863d19d19ef",
+    "url": "/static/js/3.aac8fb95.chunk.js"
+  },
+  {
+    "revision": "f1d3c41afde386cd6150",
+    "url": "/static/js/4.73a9f670.chunk.js"
+  },
+  {
+    "revision": "775ec241fd3cffa4c9d6",
+    "url": "/static/js/main.94c6abaf.chunk.js"
+  },
+  {
+    "revision": "f16ef81d493b90f96690",
+    "url": "/static/js/runtime-main.14201106.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "37ea831bfe878889c275acb6db250a2e",
+    "url": "/static/media/iitk-logo.37ea831b.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "16957b9333ddd97734fcdbb077f62cba",
+    "url": "/static/media/mastercard-logo.16957b93.png"
+  },
+  {
+    "revision": "48835b451c27e1a8681a9acf08ce08da",
+    "url": "/static/media/msrit-logo.48835b45.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/static/media/talksCardBack.39eff4a9.svg"
+  }
+]);
